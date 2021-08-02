c54a5e0c0036fc376f73a16a69761dc08bbd537a
fhideous <kul.leshiy@yandex.ru>
2021-08-02 04:12:02 +0300

    Feat(uart_tx): Формирование посылки
    Все же посылка всегда должна
    отправляться всегда именно со
    стартового бита и только после
    встречи ready && valid, так что
    переписал все на что-то более разумное

--- rtl/uart_tx.v
+++ rtl/uart_tx.v
@@ -4,14 +4,14 @@
 module uart_tx(
 
   input           clk_i,
-	input           nreset_i,
+  input           nreset_i,
 
-	input  [7:0]    tx_data_i,
+  input  [7:0]    tx_data_i,
 
   input           ready,
   output          valid,
 
-	output wire     tx_o
+  output reg     tx_o
 
 );
 
@@ -24,11 +24,12 @@ localparam  COUNTER_LEN     = 1 + $clog2(CLKS_PER_BIT);
 
 reg  [COUNTER_LEN - 1:0]  clk_counter;
 
-reg  [9:0]                reg_data;                    
+reg   [8:0]               reg_data;                    
+reg   [3:0]               n_byte;
 
-reg                      is_transmitting;
+reg                       is_transmitting;
 
-wire                    cnt_tic;
+wire                      cnt_tic;
 
 
 assign    valid   =   ( nreset_i ) && ( !is_transmitting ) ;
@@ -36,42 +37,46 @@ assign    valid   =   ( nreset_i ) && ( !is_transmitting ) ;
 always @ ( posedge clk_i ) begin 
   if ( !nreset_i ) begin
       is_transmitting    <=  1'b0;
-      reg_data           <=  10'b11_1111_1111;
-  end else if ( valid && ready ) begin 
+      reg_data           <=  9'b1_1111_1111;
+  end else if ( valid && ready  ) begin 
       is_transmitting    <= 'b1;
-      reg_data           <=  {1'b1, tx_data_i, 1'b0};
-  end
+      reg_data           <=  {1'b1, tx_data_i};
+  end else if ( n_byte == 'd9 && cnt_tic  )
+      is_transmitting    <= 'b0;
 end 
+
 
 always @ ( posedge clk_i ) begin 
   if ( !nreset_i )
     clk_counter   <= 'b0;
-  else begin
-    if ( clk_counter == CLKS_PER_BIT )
+  else if ( is_transmitting ) begin
+    if ( ( clk_counter == CLKS_PER_BIT ) )
       clk_counter <= 1'b0; 
     else 
       clk_counter <= clk_counter + 1'b1;
-  end    
+  end 
 end  
 
-reg   [3:0]           n_byte;
 
 assign        cnt_tic = ( clk_counter == CLKS_PER_BIT - 1);
 
 always @( posedge clk_i ) begin
-  if ( !nreset_i  )
+  if ( !nreset_i || ( ready && valid ))
     n_byte                <= 4'b0;
   else begin 
-    if ( n_byte == 'd9 && cnt_tic ) begin
-      n_byte              <= 4'b0;
-      is_transmitting     <= 1'b0;
-      reg_data            <= 10'b11_1111_1111;
-    end else if ( cnt_tic )
+    if ( cnt_tic )
       n_byte              <= n_byte + 1'b1; 
    end
 end
 
+always @ ( posedge clk_i ) begin
+    if ( !nreset_i || ( cnt_tic && ( n_byte == 'd9 ) ))
+        tx_o    <=      1'b1;
+    else if ( ready && valid )
+        tx_o    <=      1'b0;
+    else if ( cnt_tic )
+        tx_o   <=      reg_data[n_byte];
+end 
 
-assign tx_o = reg_data[n_byte];
 
 endmodule

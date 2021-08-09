21b9479ce4a74e9f58968c56a29025f94f46baa9
Кулешов Владислав <kul.leshiy@yandex.ru>
2021-08-09 15:59:10 +0300

    Fix (uart_tx) Исправления в работе модуля
    Теперь tx не отправляет последнее
    сообщение повторно, пока не получит
    новое. Инвертировал valid,
    ранее понимал его в обратном смысле

--- rtl/uart_tx.v
+++ rtl/uart_tx.v
@@ -18,23 +18,37 @@ localparam  COUNTER_LEN     = 1 + $clog2(CLK_PER_BIT);
 
 reg  [COUNTER_LEN - 1:0]  clk_counter;
 
-reg   [8:0]               reg_data;                    
+reg   [9:0]               reg_data;                    
 reg   [3:0]               n_bit;
 
 reg                       is_transmitting;
 
 wire                      cnt_tic;
 
-assign    valid_o   =   ( nreset_i ) && ( !is_transmitting ) ;
+assign    valid_o   =   ( nreset_i ) && ( is_transmitting ) ;
+
+
+reg                       is_new_message;
+
+always @( posedge clk_i ) begin
+  if ( !nreset_i ) 
+    is_new_message <= 'b0;
+  else begin  
+    if ( !ready_i )
+      is_new_message <= 'b1;
+    else 
+      is_new_message <= 'b0; 
+  end
+end
 
 always @( posedge clk_i ) begin 
   if ( !nreset_i ) begin
       is_transmitting    <=  1'b0;
-      reg_data           <=  9'b1_1111_1111;
-  end else if ( valid_o && ready_i ) begin 
+      reg_data           <=  10'b1_1111_1111_0;
+  end else if ( !is_transmitting && ready_i && is_new_message) begin 
       is_transmitting    <= 'b1;
-      reg_data           <=  { 1'b1, tx_data_i };
-  end else if ( n_bit == 'd9 && cnt_tic  )
+      reg_data           <=  { 1'b1, tx_data_i, 1'b0};
+  end else if ( n_bit == 'd10 && cnt_tic  )
       is_transmitting    <= 'b0;
 end 
 
@@ -42,31 +56,37 @@ always @( posedge clk_i ) begin
   if ( !nreset_i )
     clk_counter   <= 'b0;
   else if ( is_transmitting ) begin
-    if ( ( clk_counter == CLK_PER_BIT ) )
+    if ( ( clk_counter == CLK_PER_BIT ) || ( n_bit == 'd10 && cnt_tic ) )
       clk_counter <= 1'b0; 
     else 
       clk_counter <= clk_counter + 1'b1;
   end 
 end  
 
-assign        cnt_tic = ( clk_counter == CLK_PER_BIT - 1);
+assign        cnt_tic = ( clk_counter == CLK_PER_BIT - 1) || ( ( !clk_counter ) && ( is_transmitting ) 
+                                                                                && ( !n_bit )  );
 
 always @( posedge clk_i ) begin
-  if ( ( !nreset_i ) || ( ready_i && valid_o ))
+  if ( ( !nreset_i ) )
     n_bit    <= 4'b0;
   else begin 
-    if ( cnt_tic )
+    if (      ( ( n_bit == 'd10 )      && ( cnt_tic          ) )
+             || ( ready_i              && ( !is_transmitting ) ) )
+      n_bit  <= 'b0; 
+    else if ( cnt_tic )
       n_bit  <= n_bit + 1'b1; 
    end
 end
 
 always @ ( posedge clk_i ) begin
-    if ( ( !nreset_i ) || ( cnt_tic && ( n_bit == 'd9 ) ) )
+    if ( !nreset_i )
         tx_o  <=    1'b1;
-    else if ( ready_i && valid_o )
-        tx_o  <=    1'b0;
-    else if ( cnt_tic )
+    else begin
+      if ( ( !is_transmitting ) || ( cnt_tic && ( n_bit == 'd10 ) ) )
+        tx_o  <=    1'b1;
+      else if ( cnt_tic )
         tx_o  <=    reg_data[n_bit];
-end 
+      end 
+end
 
 endmodule

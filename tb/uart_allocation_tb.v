f53116d7e75bfd9a67c512163a6948bfe36f5401
Кулешов Владислав <kul.leshiy@yandex.ru>
2021-08-27 13:09:18 +0300

    Fix (uart_alloc) Переосмыслил использование модуля
    Добавил сквозных сигналов clk/reset, плюс пары valid/ready
    для связи с отправителем и получателем данных
    Выходы внутренних uart_tx/rx также вывел наружу

--- tb/uart_allocation_tb.v
+++ tb/uart_allocation_tb.v
@@ -2,30 +2,53 @@
 
 module uart_allocation_tb;
     
-    reg      clk;
-    reg      reset;
+    reg         clk_i;
+  reg         nreset_i;
+  wire        aresetn_o;
+  wire        clk_o;
 
-    reg      rx_i;
-    wire     tx_o;
-    
-    localparam CLK_SEMIPERIOD = 5;
-    
-uart_allocation uut
-(
+  reg         s_valid_i;
+  reg         m_ready_i;
 
-  .clk      (clk        ),
-  .reset    (reset      ),
+  wire        s_ready_o;
+  wire        m_valid_o;
 
-  .rx_i     (rx_i       ),
-  .tx_o     (tx_o       )
+  reg         rx_i;
+  wire  [7:0] rx_o;
 
+  reg   [7:0] tx_i;
+  wire        tx_o;
+
+
+localparam CLK_SEMIPERIOD = 5;
+
+uart_allocation #(
+  .BIT_RATE       (9600            ),
+  .CLK_HZ         (100_000_000     )
+)uut (
+  .clk_i          (clk_i      ),
+  .aresetn_i      (nreset_i   ),
+
+  .clk_o          (clk_o      ),
+  .aresetn_o      (aresetn_o  ),
+
+  .s_valid_i      (s_valid_i  ),
+  .s_ready_o      (s_ready_o  ),
+  .s_rx_data_i    (rx_i       ),
+  .s_rx_data_o    (rx_o       ),
+
+  .m_valid_o      (m_valid_o  ),
+  .m_ready_i      (m_ready_i  ),
+  .m_tx_data_o    (tx_o       ),  
+  .m_tx_data_i    (tx_i       )
 
 );
 
+
   initial begin 
-     clk = 'b0;
+     clk_i = 'b0;
      forever begin
-       #CLK_SEMIPERIOD clk = ~clk;
+       #CLK_SEMIPERIOD clk_i = ~clk_i;
      end
   end
 
@@ -37,20 +60,24 @@ integer   CLKS_PER_BIT    = 100_000_000_0 / 9600;
  initial begin 
 
 
-  reset = 'b0;
+  nreset_i = 'b0;
+  tx_i = 8'b1;
+  m_ready_i = 'b0;
   rx_i = 1'b1;
   data = 8'b1;
  #5000;
-  reset = 'b1;
+  nreset_i = 'b1;
  #5000;
 
   data = 8'b1000_0001;
-      rx_i = 1'b1;
+    rx_i = 1'b1;
+    s_valid_i = 1'b1;
       #2000;
 
       //start bit
   rx_i = 1'b0;
   #CLKS_PER_BIT;
+  s_valid_i = 1'b0;
       //data
   rx_i = data[0];
   #CLKS_PER_BIT;
@@ -71,12 +98,13 @@ integer   CLKS_PER_BIT    = 100_000_000_0 / 9600;
   // stop bit
   rx_i = 1'b1;
   #CLKS_PER_BIT;
-
+    s_valid_i = 1'b1;
   data = 8'b0010_0110;
 
       //start bit
   rx_i = 1'b0;
   #CLKS_PER_BIT;
+      s_valid_i = 1'b0;
       //data
   rx_i = data[0];
   #CLKS_PER_BIT;
@@ -143,8 +171,30 @@ integer   CLKS_PER_BIT    = 100_000_000_0 / 9600;
   #CLKS_PER_BIT;
   // stop bit
   rx_i = 1'b1;
+  
+  #CLKS_PER_BIT;
+  tx_i = 8'b1001_1101;
+  #CLKS_PER_BIT;
+  #CLKS_PER_BIT;
+  
+  m_ready_i = 1'b1;
+  #CLKS_PER_BIT;
+  #CLKS_PER_BIT;
+  #CLKS_PER_BIT;
+  #CLKS_PER_BIT;
+  #CLKS_PER_BIT;
+  #CLKS_PER_BIT;
+  #CLKS_PER_BIT;
+  #CLKS_PER_BIT;
+  #CLKS_PER_BIT;
+  #CLKS_PER_BIT;
   #CLKS_PER_BIT;
 
+  #CLKS_PER_BIT;
+  #CLKS_PER_BIT;
+  m_ready_i = 1'b0;
+  #CLKS_PER_BIT;
+  m_ready_i = 1'b1;
 
  end
 
